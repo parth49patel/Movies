ca9ed3dd37bd6977a07c2e742e1a3e261fac447a
Parth Patel <136750327+parth49patel@users.noreply.github.com>
2026-07-13 20:19:22 -0400

    Initial Commit

---
+++ Movies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by Parth Patel on 2026-07-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Parth Patel on 2026-07-13.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
