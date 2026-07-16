ec5a4ea505ec948c7fcba04f0a212e12b9a527dd
Parth Patel <136750327+parth49patel@users.noreply.github.com>
2026-07-15 21:10:55 -0400

    Add networking layer: models, endpoint enum, network error enum, service, and mock

---
+++ Movies/Networking/APIConstants.swift
@@ -0,0 +1,8 @@
+//
+//  APIConstants.swift
+//  Movies
+//
+//  Created by Parth Patel on 2026-07-15.
+//
+
+import Foundation
---
+++ Movies/Networking/TMDBServiceProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  TMDBServiceProtocol.swift
+//  Movies
+//
+//  Created by Parth Patel on 2026-07-15.
+//
+
+import Foundation
